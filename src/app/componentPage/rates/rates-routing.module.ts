0633971db6301326214b650a713efc9d6afd9053
刘刚 <1215951887@qq.com>
2018-10-12 14:16:05 +0800

    路由简写更新

--- src/app/componentPage/rates/rates-routing.module.ts
+++ src/app/componentPage/rates/rates-routing.module.ts
@@ -1,14 +1,7 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
 import {RatesComponent} from './rates.component';
 
-const routes: Routes = [{
+export const RatesRoutes = [{
   path:"rates",
   component:RatesComponent
 }];
 
-@NgModule({
-  imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
-})
-export class RatesRoutingModule { }
--- src/app/componentPage/rates/rates.module.ts
+++ src/app/componentPage/rates/rates.module.ts
@@ -1,16 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {RatingModule} from 'ngx-bootstrap';
-import { RatesRoutingModule } from './rates-routing.module';
+import {RouterModule} from '@angular/router';
+import { RatesRoutes } from './rates-routing.module';
 import { RatesComponent } from './rates.component';
- import {FormsModule} from '@angular/forms';
+import {FormsModule} from '@angular/forms';
+import {RouterModule} from '@angular/router';
 
 @NgModule({
   imports: [
     CommonModule,
     RatingModule.forRoot(),
-    FormsModule,
-    RatesRoutingModule
+    RouterModule.forChild(RatesRoutes),
+    FormsModule
   ],
   declarations: [RatesComponent]
 })
